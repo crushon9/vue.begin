53e8e65747c415115162e812a7e3b2cf3d2bb7a5
crushon9 <catco929@naver.com>
2024-05-30 14:51:21 +0900

    cli start

--- learn-vue-js/vue2-cli/src/App.vue
+++ learn-vue-js/vue2-cli/src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- template 아래에는 하나의 상위 태그만 있어야함 -->
+  <div>
+    <h1>vue-cli : Command Line Interface</h1>
+    <h5>------App start--------</h5>
+    <p>{{ parentdata }}</p>
+    <p>{{ parentdata2 }}</p>
+    <app-header v-bind:propsname="parentdata" v-bind:propsname2="parentdata2" v-on:renew="renewData" />
+    <h5>------App end--------</h5>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
 
 export default {
-  name: 'App',
+  // 객체 리터럴방식으로 쓰면 new Vue()로 만든값과 충돌할수있기때문에
+  // export default에서는 함수 리턴으로 값을 넣음
+  data: function() {
+    return {
+      parentdata: 'data from app.vue',
+      parentdata2: 'parentdata',
+      cnt:0
+    }
+  },
   components: {
-    HelloWorld
+    'app-header':AppHeader,
+  },
+  methods: {
+    renewData: function() {
+      this.parentdata2 = '"renew" event $emit by <AppHeader> X '+ (++this.cnt)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ learn-vue-js/vue2-cli/src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <h5>------AppHeader start--------</h5>
+        <p>props : {{ propsname }}</p>
+        <p>props : {{ propsname2 }}</p>
+        <button v-on:click="sendEvent">send</button>
+        <h5>------AppHeader end--------</h5>
+    </header>
+</template>
+
+<script>
+export default {
+    props: ['propsname','propsname2'],
+    // props: {
+    //     이름:타입
+    // }
+    methods: {
+        sendEvent: function() {
+            this.$emit('renew')
+        // 이벤트 받는곳 : app-header 태그를 호출한 부모태그의 v-on:renew
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
