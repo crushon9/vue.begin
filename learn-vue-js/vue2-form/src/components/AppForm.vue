0822dd10b2e55bdb6c0acab199d018ed4f1c4592
crushon9 <catco929@naver.com>
2024-05-30 15:26:42 +0900

    update

---
+++ learn-vue-js/vue2-form/src/components/AppForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <form action="">
+    <div>
+        <label for="username">id : </label>
+        <input id="username" type="text" v-model="username">
+    </div>
+    <div>
+        <label for="password">pw : </label>
+        <input id="password" type="text">
+    </div>
+    <button>login</button>
+  </form>
+</template>
+
+<script>
+export default {
+    // 컴포넌트간 데이터의 참조가 일어나지않게 새객체를 리턴해줌
+    data: function() { 
+        return {
+            username:'',
+            password:''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
